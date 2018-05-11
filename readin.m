e7b34b327b0cd800c71fb628b0005aaa7c295281
asemple243 <asemple243@gmail.com>
2018-05-11 14:19:32 -0500

    Update readin.m

--- readin.m
+++ readin.m
@@ -1,3 +1,7 @@
+% 1) unzip the .gz file: gunzip -k velo.gz , which will produce output velo
+% 2) replace spaces with commas in velo: sed -i -e 's/ /,/g' velo
+% 3) run the readin file which creates separate x, y, z and T vectors
+clear m fname visc_capALL 
 %close all
 %clear all
 tic
@@ -5,5 +9,40 @@ fname=dir('visc.*0');
 [m,~]=size(fname);
 for i=1:m
 visc_capALL(:,i)= csvread(fname(i,1).name,2,0);
+
 end
 toc
+% % %tracer0=csvread('tracer.0.0');
+% fulltracer0= csvread(fname(1,1).name,2,0);
+% fulltracer1= csvread(fname(2,1).name,2,0);
+% fulltracer10= csvread(fname(3,1).name,2,0);
+% fulltracer11= csvread(fname(4,1).name,2,0);
+% fulltracer12= csvread(fname(5,1).name,2,0);
+% fulltracer13= csvread(fname(6,1).name,2,0);
+% fulltracer14= csvread(fname(7,1).name,2,0);
+% fulltracer15= csvread(fname(8,1).name,2,0);
+% fulltracer16= csvread(fname(9,1).name,2,0);
+% fulltracer17= csvread(fname(10,1).name,2,0);
+% fulltracer18= csvread(fname(11,1).name,2,0);
+% fulltracer19= csvread(fname(12,1).name,2,0);
+% fulltracer2= csvread(fname(13,1).name,2,0);
+% fulltracer20= csvread(fname(14,1).name,2,0);
+% fulltracer21= csvread(fname(15,1).name,2,0);
+% fulltracer22= csvread(fname(16,1).name,2,0);
+% fulltracer23= csvread(fname(17,1).name,2,0);
+% fulltracer3= csvread(fname(18,1).name,2,0);
+% fulltracer4= csvread(fname(19,1).name,2,0);
+% fulltracer5= csvread(fname(20,1).name,2,0);
+% fulltracer6= csvread(fname(21,1).name,2,0);
+% fulltracer7= csvread(fname(22,1).name,2,0);
+% fulltracer8= csvread(fname(23,1).name,2,0);
+% fulltracer9= csvread(fname(24,1).name,2,0);
+
+%%
+clear visc_sm
+for k=1:96
+    for j=1:33
+        visc_sm(j,k)=mean(visc_capALL(j:33:end,k));
+    end
+end
+visc_avg_n19amb165=mean(visc_sm(:,2:2:end),2);
